03f599219277dcb551d52c608f2e03eca3835f2d
Don Nnguyen <donng24@gmail.com>
2022-11-13 14:05:02 -0600

    added score and gameover function

--- arcade/app.js
+++ arcade/app.js
@@ -3,30 +3,34 @@
 // states after this depends on number of fruit eaten and then how many tails
 //
 //ctx stands for 'context', used in pairing with canvas to mean whatever
-//constants
-// const up = {x: 0, y: -1}
-// const down = {x: 0, y: 1 }
-// const left = {x: 1, y: 0 }
-// const right = {x: -1, y: 0 }
+
 
 //the board
 const canvas=document.getElementById('gameboard'); 
 const ctx=canvas.getContext('2d')
 
-function drawGame(){
-    clearScreen();
+class snakePart{
+    constructor(x, y){
+        this.x=x;
+        this.y=y; //create new object and set value for existing objects, aka making sure the tail parts are in the right place
+    }
 }
+
+// constants
+let speed = 7;
+let score = 0;
+
+
+let appleX = 5;
+let appleY = 5;
+
+const snakeTail=[];
+let tailLength = 2;
+
+//refresh
 function clearScreen(){
     ctx.fillStyle='black'
     ctx.fillRect(0,0,canvas.clientWidth, canvas.clientHeight)
-}
-
-//drawGame();
-
-//refresh
-function drawGame(){
-    let speed = 7;
-    setTimeout(drawGame, 1000/speed);
 }
 
 //the snake
@@ -37,164 +41,153 @@ let headY=10;
 
 function drawSnake(){
     ctx.fillStyle="green";
-  ctx.fillRect(headX* tileCount,headY* tileCount, tileSize,tileSize)
+  ctx.fillRect(headX*tileCount,headY*tileCount, tileSize,tileSize);
 
+  for (let i=0; i<snakeTail.length; i++){
+    let part = snakeTail[i]
+    ctx.fillRect(part.x*tileCount, part.y*tileCount, tileSize, tileSize)
+  }
+  snakeTail.push(new snakePart(headX, headY));
+    if(snakeTail.length>tailLength){
+        snakeTail.shift();
+ }
 }
 
-//
+//snake direction w/arrow keys
 let xvelocity=0;
 let yvelocity=0;
+
+
+document.body.addEventListener('keydown', keyDown);
 
 function changePosition(){
     headX=headX + xvelocity;
     headY=headY + yvelocity;
 }
 
-
- //snake direction w/ arrow keys
- document.body.addEventListener('downArrow', keyDown);
-
  function keyDown(event)
-
  //up
  {
      if(event.keyCode==38){
-         yvelocity=-1; //move one tile up
+         if(yvelocity==1) //keycode corresponds to the arrow keys, whenver pressed it will change direction of snake
+         return;
+         yvelocity=-1;
          xvelocity=0;
  
      }
      //down
      if(event.keyCode==40){
-         yvelocity=1;//move one tile down
+         if(yvelocity==-1)
+         return;
+         yvelocity=1;
          xvelocity=0;
      }
  
  //left
      if(event.keyCode==37){
+         if(xvelocity==1)
+         return;
          yvelocity=0;
-         xvelocity=-1;//move one tile left
+         xvelocity=-1;
      }
-     //right
-     if(event.keyCode==39){
+    console.log(event.keyCode)
+     //right 
+     if(event.keyCode==39){console.log('hello')
+         if(xvelocity==-1)
+         return;
          yvelocity=0;
-         xvelocity=1;//move one tile right
+         xvelocity=1;
      }
  }
-
-//  function keyDown(event)
-//  //up
-//  {
-//      if(event.keyCode==38){
  
-//          if(yvelocity==1)
-//          return; //prevent snake from moving in opposite direction
-//          yvelocity=-1;
-//          xvelocity=0;
- 
-//      }
-//      //down
-//      if(event.keyCode==40){
-//          if(yvelocity==-1)
-//          return;//prevent snake from moving in opposite direction
-//          yvelocity=1;
-//          xvelocity=0;
-//      }
- 
-//  //left
-//      if(event.keyCode==37){
-//          if(xvelocity==1)
-//          return;//prevent snake from moving in opposite direction
-//          yvelocity=0;
-//          xvelocity=-1;
-//      }
-//      //right
-//      if(event.keyCode==48){
-//          if(xvelocity==-1)
-//          return;//prevent snake from moving in opposite direcction
-//          yvelocity=0;
-//          xvelocity=1;
-//      }
-//  }
-
- function changeSnakePosition(){
-    headX=headX + xvelocity;
-    headY=headY+ yvelocity;
-
-}
-
 
 //food 
-let appleX = 5;
-let appleY = 5;
-
 function apple(){
     ctx.fillStyle = "red";
-    ctx.fillRect(100, 100, 18, 18)
+    ctx.fillRect(appleX*tileCount, appleY*tileCount, tileSize, tileSize)
 }
 
 
 //when snake head collides with apple
-const snakeTail=[];
-let tailLength = 2;
+function keepScore(){ //score stuff
+    ctx.fillStyle="white"
+    ctx.font="20px verdena"
+    ctx.fillText("Score: " +score, canvas.clientWidth-100,15);
+}
 
 function headCollision(){
-    if(appleX==headX && appleY==headY){
-        appleX=Math.floor(math.random()*tileCount);
+    if(appleX==headX && appleY==headY){ //if apple and head of snake is same location, add to tail and add to score
+        appleX=Math.floor(Math.random()*tileCount); 
         appleY=Math.floor(Math.random()*tileCount);
+
         tailLength++;
+        score++;
     }
 }
 
-class snakePart{
-    constructor(x, y){
-        this.x=x;
-        this.y=y;
+//game over function if hits wall
+function gameOver(){
+    let checkgameOver=false;
+    if(yvelocity===0 && xvelocity===0){
+        return false;
     }
+    if(headX<0){ //game over if snake hits left wall
+        checkgameOver=true;
+    }
+    else if(headX===tileCount){ //game over if snake hits right wall
+        checkgameOver=true;
+    }
+    else if(headY<0){// game over if snake hits top wall
+        checkgameOver=true;
+    }
+    else if (headY===tileCount){ //game over if snake hits bottom wall
+        checkgameOver=true;
+    }
+
+    //game over function if hits itself
+    for(let i=0; i<snakeTail.length; i++){
+    let part = snakeTail[i]
+    if(part.x === headX && part.y === headY){ // if any part of the snake is occupying same space, if so then game over
+        checkgameOver = true;
+        break; //break out of for loop
+    }
+    }
+
+    if(checkgameOver){
+        ctx.fillStyle = "white";
+        ctx. font = "20px verdana";
+        ctx.fillText ("Game Over! Your score was:" + score, canvas.clientWidth/7.5, canvas.clientHeight/2); //position of game over text
+    }
+    return checkgameOver;
+    }
+
+// reset the game
+function restart(){
+    document.location.reload(false);
 }
 
-function drawSnake(){
-    ctx.fillStyle="green";
-    for (let i=0; i<snakeTail.length; i++){
-        let part = snakeTail[i]
-        ctx.fillRect(part.x*tileCount, part.y*tileCount, tileSize, tileSize)
-
-    }
-    snakeTail.push(new snakePart(headX, headY));
-}
 
 
-
-
-
-
-
+///////////////////////////////////////////////////////////////////////
+//function where the whole game is called
 function drawGame(){
     clearScreen();
    drawSnake();
    changePosition();
+
+   let result = gameOver();
+   if(result){
+    return;
+   }
+
    apple();
    headCollision();
+   keepScore();
+
+   setTimeout(drawGame, 1000/speed);
+
+   console.log(snakeTail)
 }
- 
+
+
 drawGame();
-
-
-//states
-// let state = {
-//     fruit: [6, 9],
-//     snake: snake
-// }
-
-// //snake
-// let snake = {
-//     body: 
-//     nextDirection [1, 0]
-// }
-
-// // define my states and variables
-
-// // fruit- make random appearance on board and disappear when "eaten"
-
-// // snake- how long and what direction to start off
-
-// // homepage- start button and score, goes up everytime head eats fruit
